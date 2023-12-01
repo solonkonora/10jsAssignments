1ff85a902d3d2e03654b8e5da6b9da6bacaa0005
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-11-30 22:39:26 -0800

    solving linter errors

--- isEven.js
+++ isEven.js
@@ -1,7 +1,7 @@
 // 1)write a function called isEven which checks if a number is even or odd
 // Hint: 0 is even and 1 is odd, for any other nummber N, its eveness is the same as N-2.
 // Notice how this works when N is negative and try to fix it.
-function isEven(p) {
+function isEven (p) {
   if (p % 2 === 0) {
     console.log(p + ' is even')
   } else {
@@ -10,42 +10,41 @@ function isEven(p) {
 }
 isEven(2)
 
-// // 2) Write a function called countBs that takes a string as its only argument and returns a number that indicates how many uppercase "B" characters are in the string
+// 2) Write a function called countBs that takes a string as its only argument and returns a number that indicates how many uppercase "B" characters are in the string
 // function countBs (string) {
 // }
 // 3) function countChar that behaves like countBs expect it takes a second argument that indicates the character that is to be  counted
-//4) function to generate the first n fibonacci numbers (fib=sum of the first 2 previous numbers)
-function fibonacci(n) {
+// 4) function to generate the first n fibonacci numbers (fib=sum of the first 2 previous numbers)
+function fibonacci (n) {
 const fibonacci = [0, 1]
   for (let i = 2; i < n; i++) {
     fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]
   }
-return fibonacci.slice(0, n)
+  return fibonacci.slice (0, n)
 }
 console.log(fibonacci(5))
 
-//6)sum function that takes an array and returns the sum of its elements
-//sum([1,2,3,4,5]) => 15
+// 6)sum function that takes an array and returns the sum of its elements
+// sum([1,2,3,4,5]) => 15
 function sum (num) {
-let sum = 0
+  let sum = 0
   for (let c = 0; c < num.length; c++) {
     sum += num[c];
+  }
+  return sum
 }
-return sum
-}
-console.log(sum([1,2,3,4,5]))
+console.log(sum([1, 2, 3, 4, 5]))
 
-//7)Write a function to return the average of an array
-//average([1,2,3,4,5]) => 3
-function calculateAverage(numbers) {
-let sum = 0
+// 7)Write a function to return the average of an array
+// average([1,2,3,4,5]) => 3
+function calculateAverage (numbers) {
+  let sum = 0
   for (let b = 0; b < numbers.length; b++) {
     sum += numbers[b]
   }
-const average = sum / numbers.length
-return average;
+  const average = sum / numbers.length
+  return average;
 }
 const arrayOfNumbers = [1, 2, 3, 4, 5]
 const avg = calculateAverage(arrayOfNumbers)
 console.log(avg)
-
--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-function sum (num) {
-//     let sum = 0
-//     for (let i = 0; i < num.length; i++) {
-//         sum += num[i];
-//     }
-//     return sum
-// }
-// console.log(sum([1,2,3,4,5]))
