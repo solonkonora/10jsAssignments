9712a3b8e9477443e93dfa7ccb2cf3f8cf74afd0
Nkwada Nora Solonko <nkwadanora@mail.com>
2023-11-30 16:00:44 -0800

    solving linter errors

--- isEven.js
+++ isEven.js
@@ -2,10 +2,10 @@
 // Hint: 0 is even and 1 is odd, for any other nummber N, its eveness is the same as N-2.
 // Notice how this works when N is negative and try to fix it.
 function isEven(n) {
- if (n % 2 == 0) {
-    console.log(n + " is even")
- } else {
-   console.log(n + " is odd")
- }
+    if (n % 2 == 0) {
+        console.log(n + ' is even')
+    } else {
+        console.log(n + ' is odd')
+    }
 }
 isEven(2)
